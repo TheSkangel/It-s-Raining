9e02e97fae8cbf30dcbac59d4f5487f3020237cb
Jaruz <jaruz.cd@gmail.com>
2017-01-15 14:55:30 -0500

    tweaks
    yeyey

---
+++ GameJamMenu/Assets/Game Jam Menu Template/Prefabs/Items/ItemPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPlatform : MonoBehaviour {
+
+    public int yValueToKillSelf = -7;
+
+	void Start () {
+
+        Physics2D.IgnoreLayerCollision(9, 16);
+
+	}
+	
+	void Update () {
+
+        if (GameController.state != "playing")
+            return;
+
+        //kill when below certain y value
+        if (transform.position.y < yValueToKillSelf)
+            DestroyPlatform();
+
+	}
+
+    void DestroyPlatform() {
+
+        Destroy(gameObject);
+
+    }
+
+}
---
+++ GameJamMenu/Assets/Game Jam Menu Template/Scripts/ItemSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour {
+
+    public GameObject platformPrefab;
+
+    public float spawnRate = 10f;
+    float spawnTimer;
+
+    public float xSpawnOffset = -10;
+
+    void Start() {
+
+        spawnTimer = Time.time + spawnRate;
+
+    }
+
+	void Update () {
+		
+        if(Time.time > spawnTimer) {
+
+            SpawnPlatform();
+
+            spawnTimer = Time.time + spawnRate;
+
+        }
+
+	}
+
+    void SpawnPlatform() {
+
+        float offSet = Random.Range(-xSpawnOffset, xSpawnOffset);
+
+        GameObject newPlatform = Instantiate(platformPrefab, transform.position + new Vector3(offSet, 0, 0), Quaternion.identity) as GameObject;
+
+    }
+
+    void ResetValues() {
+
+        spawnTimer = Time.time + spawnRate;
+
+    }
+
+}
